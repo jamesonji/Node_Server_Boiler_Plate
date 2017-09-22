b002310de41d65c62abb59ab4c169353e09bafd5
Song Ji <songji0509@gmail.com>
2017-09-22 09:11:52 -0700

    Change project structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  entry: './client/App.js',
+  entry: './client/index.js',
   output: {
     path: path.resolve('./public/javascripts'),
     filename: 'bundle.js'
