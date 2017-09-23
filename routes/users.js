884facb85c7b19503319116da7bbea3f7eeb02b6
Song Ji <songji0509@gmail.com>
2017-09-23 12:28:19 -0700

    Add routes comment and install mongoose

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 // get a list of users
+// get: localhost/users
 router.get('/', function(req, res, next) {
   res.render('users/index');
 });
 
 // get to the create user page
+// get : localhost/users/new
 router.get('/new', function(req, res, next) {
   res.render('users/new');
 });
 
 // add a new user to db
-router.post('/users', function(req, res, next) {
-  res.redirect('/users');
+// post: locahost/users
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.send({
+    type:'POST',
+    name: req.body.name,
+    rank: req.body.rank
+  })
 });
 
 // update a user in the db
-router.put('/users/:id', function(req, res, next)){
+// put: localhost: users/:id
+router.put('/:id', function(req, res, next) {
   res.send({type: 'PUT'});
-}
+});
 
 // delete a user in the db
-router.delete('/users/:id', function(req, res, next)){
+// delete: localhost: users/:id
+router.delete('/:id', function(req, res, next) {
   res.send({type:'DELETE'});
-}
+});
 module.exports = router;
