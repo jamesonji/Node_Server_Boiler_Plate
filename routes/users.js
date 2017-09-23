09c01299465e06438b701d9a5d4e6e218b01631c
Song Ji <songji0509@gmail.com>
2017-09-23 12:08:41 -0700

    Finish user request structures

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// get a list of users
 router.get('/', function(req, res, next) {
   res.render('users/index');
 });
 
+// get to the create user page
 router.get('/new', function(req, res, next) {
   res.render('users/new');
 });
 
+// add a new user to db
 router.post('/users', function(req, res, next) {
   res.redirect('/users');
 });
 
+// update a user in the db
+router.put('/users/:id', function(req, res, next)){
+  res.send({type: 'PUT'});
+}
+
+// delete a user in the db
+router.delete('/users/:id', function(req, res, next)){
+  res.send({type:'DELETE'});
+}
 module.exports = router;
