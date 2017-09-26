ebcda4a438b544009b0569827e8e4698d33fd054
Song Ji <songji0509@gmail.com>
2017-09-26 15:32:33 -0700

    Add user max login lock

--- model/users.js
+++ model/users.js
@@ -2,33 +2,30 @@ const mongoose = require('mongoose'),
       Schema = mongoose.Schema,
       bcrypt = require('bcrypt'),
       SALT_WORK_FACTOR = 10;
+      // these values can be whatever you want - we're defaulting to a
+      // max of 5 attempts, resulting in a 2 hour lock
+      MAX_LOGIN_ATTEMPTS = 5,
+      LOCK_TIME = 2 * 60 * 60 * 1000;
 
 //create user Schema & model
 const UserSchema = new Schema({
   username:{
     type: String,
-    required: [true, 'User name is required']
+    required: [true, 'User name is required'],
+    index: { unique: true }
   },
-  fname: {
-    type: String,
-    default: "",
-  },
-  lname: {
-    type: String,
-    default: ""
-  },
-  password: {
-    type: String, 
-    required: true 
-  },
-  dob:{
-    type: Date,
-    default: Date.now()
-  },
-  available:{
-    type: Boolean,
-    default: false
-  }
+  fname: {type: String, default: ""},
+  lname: {type: String, default: ""},
+  password: {type: String, required: true },
+  dob:{type: Date, default: Date.now()},
+  available:{type: Boolean, default: false},
+  loginAttempts: { type: Number, required: true, default: 0 },
+  lockUntil: { type: Number }
+});
+
+UserSchema.virtual('isLocked').get(function() {
+    // check for a future lockUntil timestamp
+    return !!(this.lockUntil && this.lockUntil > Date.now());
 });
 
 UserSchema.pre('save', function(next) {
@@ -58,6 +55,77 @@ UserSchema.methods.comparePassword = function(candidatePassword, callback) {
         callback(null, isMatch);
     });
 };
+
+UserSchema.methods.incLoginAttempts = function(cb) {
+    // if we have a previous lock that has expired, restart at 1
+    if (this.lockUntil && this.lockUntil < Date.now()) {
+        return this.update({
+            $set: { loginAttempts: 1 },
+            $unset: { lockUntil: 1 }
+        }, cb);
+    }
+    // otherwise we're incrementing
+    var updates = { $inc: { loginAttempts: 1 } };
+    // lock the account if we've reached max attempts and it's not locked already
+    if (this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
+        updates.$set = { lockUntil: Date.now() + LOCK_TIME };
+    }
+    return this.update(updates, cb);
+};
+
+// expose enum on the model, and provide an internal convenience reference 
+var reasons = UserSchema.statics.failedLogin = {
+    NOT_FOUND: 0,
+    PASSWORD_INCORRECT: 1,
+    MAX_ATTEMPTS: 2
+};
+
+UserSchema.statics.getAuthenticated = function(username, password, cb) {
+    this.findOne({ username: username }, function(err, user) {
+        if (err) return cb(err);
+
+        // make sure the user exists
+        if (!user) {
+            return cb(null, null, reasons.NOT_FOUND);
+        }
+
+        // check if the account is currently locked
+        if (user.isLocked) {
+            // just increment login attempts if account is already locked
+            return user.incLoginAttempts(function(err) {
+                if (err) return cb(err);
+                return cb(null, null, reasons.MAX_ATTEMPTS);
+            });
+        }
+
+        // test for a matching password
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) return cb(err);
+
+            // check if the password was a match
+            if (isMatch) {
+                // if there's no lock or failed attempts, just return the user
+                if (!user.loginAttempts && !user.lockUntil) return cb(null, user);
+                // reset attempts and lock info
+                var updates = {
+                    $set: { loginAttempts: 0 },
+                    $unset: { lockUntil: 1 }
+                };
+                return user.update(updates, function(err) {
+                    if (err) return cb(err);
+                    return cb(null, user);
+                });
+            }
+
+            // password is incorrect, so increment login attempts before responding
+            user.incLoginAttempts(function(err) {
+                if (err) return cb(err);
+                return cb(null, null, reasons.PASSWORD_INCORRECT);
+            });
+        });
+    });
+};
+
 
 const User = mongoose.model('user', UserSchema);
 
